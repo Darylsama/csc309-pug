e66a6b81cd75e393768f9239c841a3c4dbcb3aac
xiang.li712@gmail.com <xiang.li712@gmail.com@8361de56-865f-85c7-5126-74faa11e47cb>
2012-06-29 21:35:04 +0000

    separate navbar and side bar into their own file added a few more games

--- trunk/view/create_game_page.php
+++ trunk/view/create_game_page.php
@@ -1,69 +1,60 @@
 <div class="container-fluid">
-    <div class="row-fluid">
+  <div class="row-fluid">
 
-        <!-- sidebar -->
-        <div class="span2">
-            <ul class="nav nav-pills nav-stacked">
-                <li class="active">
-                    <a href="new_game.php"> Create New Game </a>
-                </li>
-                <li>
-                    <a href="list_games.php">Browse Existing Games</a>
-                </li>
-                <li>
-                    <a href="#"> View Users </a>
-                </li>
-            </ul>
-        </div>
+    <!-- sidebar -->
+    <?php include "view/sidebar.php"?>
 
-        <!-- register form -->
-        <div class="span4 hero-unit">
-            <form method="post" action="new_game.php" name="create_game" class="form-horizontal">
-                <fieldset>
+    <!-- register form -->
+    <div class="span4 hero-unit">
+      <form method="post" action="new_game.php" name="create_game"
+        class="form-horizontal">
+        <fieldset>
 
-                    <legend>
-                        Please submit the following information:
-                    </legend>
-                    <!-- title -->
+          <legend> Please submit the following information: </legend>
+          <!-- title -->
 
-                    <div class="control-group">
-                        <label class="control-label" for="username-input">Game</label>
-                        <div class="controls">
-                            <input id="gamename-input" class="input-xlarge" name="gamename" type="text">
-                        </div>
-                    </div>
-                    <!-- gamename -->
+          <div class="control-group">
+            <label class="control-label" for="username-input">Game</label>
+            <div class="controls">
+              <input id="gamename-input" class="input-xlarge"
+                name="gamename" type="text">
+            </div>
+          </div>
+          <!-- gamename -->
 
-                    <div class="control-group">
-                        <label class="control-label" for="sport-select">Sport</label>
-                        <div class="controls">
-                            <select id="sport-select" name="sport">
-                            <?php foreach ($this->page["sports"] as $sport) { ?>
-                                <option value="<?php echo $sport->sid; ?>"><?php echo $sport->name; ?></option>
-                            <?php } ?>
-                            </select>
-                        </div>
-                    </div>
-                    <!-- sports -->
+          <div class="control-group">
+            <label class="control-label" for="sport-select">Sport</label>
+            <div class="controls">
+              <select id="sport-select" name="sport">
+                <?php foreach ($this->page["sports"] as $sport) { ?>
+                <option value="<?php echo $sport->sid; ?>">
+                  <?php echo $sport->name; ?>
+                </option>
+                <?php } ?>
+              </select>
+            </div>
+          </div>
+          <!-- sports -->
 
-                    <div class="control-group">
-                        <label class="control-label" for="description-input">Description</label>
-                        <div class="controls">
-                            <textarea name="description" rows="10"></textarea>
-                        </div>
-                    </div>
-                    <!-- Description -->
+          <div class="control-group">
+            <label class="control-label" for="description-input">Description</label>
+            <div class="controls">
+              <textarea name="description" rows="10"></textarea>
+            </div>
+          </div>
+          <!-- Description -->
 
 
-                    <div class="control-group">
-                        <input type="submit" class="btn btn-primary pull-right" value="Create" />
-                    </div>
-                    <!-- submit -->
-                </fieldset>
+          <div class="control-group">
+            <input type="submit" class="btn btn-primary pull-right"
+              value="Create" />
+          </div>
+          <!-- submit -->
+        </fieldset>
 
-            </form>
-        </div>
-
+      </form>
     </div>
+
+  </div>
 </div>
 
--- trunk/view/list_games_page.php
+++ trunk/view/list_games_page.php
@@ -1,47 +1,35 @@
 <div class="container-fluid">
   <div class="row-fluid">
 
-<div class="span2">
-  <ul class="nav nav-pills nav-stacked">
-<li>
-  <a href="new_game.php"> Create New Game </a>
-</li>
-<li class="active">
-  <a href="list_games.php">Browse Existing Games</a>
-</li>
-<li>
-  <a href="#"> View Users </a>
-</li>
-  </ul>
-</div>
+    <?php
+    include "view/sidebar.php"
+    ?>
 
-<div class="span10" >
-<table>
-  
-<thead>
-  <tr>
-<th>Name</th>
-<th>Sport</th>
-<th>Description</th>
-<th>Organizer</th>
-<th>Created On</th>
-  </tr>
-</thead>
+    <div class="span10">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Sport</th>
+            <th>Description</th>
+            <th>Organizer</th>
+            <th>Created On</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($this->page["games"] as $game) { ?>
+          <tr>
+            <td><a href="view_game.php?gid=<?php echo $game->gid; ?>"><?php echo $game -> name; ?>
+            </a></td>
+            <td><?php echo $game -> sport -> name; ?></td>
+            <td><?php echo $game -> desc; ?></td>
+            <td><?php echo $game -> organizer -> username; ?></td>
+            <td><?php echo $game -> creation; ?></td>
+          </tr>
+          <?php } ?>
+        </tbody>
 
-<tbody>
-  <?php foreach ($this->page["games"] as $game) { ?>
-  <tr>
-    <td><a href="view_game.php?gid=<?php echo $game->gid; ?>"><?php echo $game -> name; ?></a></td>
-    <td><?php echo $game -> sport -> name; ?></td>
-    <td><?php echo $game -> desc; ?></td>
-    <td><?php echo $game -> organizer -> username; ?></td>
-    <td><?php echo $game -> creation; ?></td>
-  </tr>
-  <?php } ?>
-</tbody>
-
-</table>
-</div>
-
-</div>
+      </table>
+    </div>
+  </div>
 </div>
