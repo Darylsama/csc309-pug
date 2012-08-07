e919bdda8abe7c74a3ac077aabfa1e0faebaaf13
xiang.li712 <xiang.li712@gmail.com>
2012-08-07 00:23:26 +0000

    initially disable the date picker on the view game page

--- view/js/view_game.js
+++ view/js/view_game.js
@@ -5,7 +5,7 @@ function init_view_game() {
 	var start_date = new Date(game_start.attr("data-time") * 1000);
 	
 	// create date picker widget
-	game_start.datepicker();
+	game_start.datepicker({disabled: true});
 	
 	// set the date for the widget
 	game_start.datepicker("setDate", start_date);
